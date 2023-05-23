c514cfac9a29eddfc612ee0ff4eb8513f56ff7f9
Mike Klein <mtklein@gmail.com>
2023-05-23 13:24:54 -0500

    even better

--- slab.c
+++ slab.c
@@ -2,14 +2,15 @@
 #include "vec.h"
 
 #define len(x) (int)( sizeof(x) / sizeof((x)[0]) )
-#define K len((struct State){0}.r)
 
 void run(struct Effect const effect[], int const effects, int n) {
     for (int end = 0; n;) {
+        struct State st = {0};
+        int const K = len(st.r);
+
         end += n < K ? 1 : K;
         n   -= n < K ? 1 : K;
 
-        struct State st = {0};
         for (int i = 0; i < effects; i++) {
             st = effect[i].fn(st, end, effect[i].ctx);
         }

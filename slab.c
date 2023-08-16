2f616814d27daf749de40fcf1219ac9553b15adb
Mike Klein <mtklein@gmail.com>
2023-08-16 08:48:55 -0500

    encapsulate not_yet_full() better
    Unlike using the public API function addresses as sentinels, it is safe
    to use a static, so this could be implemented as
    
        static void *not_yet_full(void) {
            return (void*)not_yet_full;
        }
    
    but that means not_yet_full() would be instantiated and take up a few
    instructions of code size, where with this not_yet_full() can always be
    inlined and we still burn only that single byte.  This is the
    archetypical "no big deal", but might as well.

--- slab.c
+++ slab.c
@@ -8,17 +8,20 @@ struct slab {
     void    *val[M];
 };
 
-static char const not_yet_full = 0;
+static void *not_yet_full(void) {
+    static char const unique_address = 0;
+    return (union {void const *cptr; void *ptr;}){&unique_address}.ptr;
+}
 
 struct slab *slab_alloc(void) {
     struct slab *s = calloc(1, sizeof *s);
-    s->val[M-1] = (union { void const *cptr; void *ptr; }){&not_yet_full}.ptr;
+    s->val[M-1] = not_yet_full();
     return s;
 }
 
 int slab_len(struct slab const *s) {
-    return s->val[M-1] == &not_yet_full ? (int)s->key[M-1]
-                                        : M;
+    return s->val[M-1] == not_yet_full() ? (int)s->key[M-1]
+                                         : M;
 }
 
 bool slab_insert(struct slab *s, intptr_t key, void *val) {

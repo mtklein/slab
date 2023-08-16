53b896d429e06a83af109aca0f749572b1f92fbc
Mike Klein <mtklein@gmail.com>
2023-08-16 09:02:27 -0500

    very basic ctx threading testing
    This is really mostly about getting some of the NULL noise cleared up,
    but also might as well test that the ctx we pass slab_lookup() is the
    ctx we receive in eq().

--- slab_test.c
+++ slab_test.c
@@ -11,8 +11,9 @@ static void test(void (*fn)(struct slab*)) {
     free(s);
 }
 
-static _Bool key_eq(intptr_t k1, intptr_t k2, void *ctx) {
-    (void)ctx;
+static void *ctx = &ctx;
+static _Bool key_eq(intptr_t k1, intptr_t k2, void *arg) {
+    expect(arg == ctx);
     return k1 == k2;
 }
 
@@ -29,24 +30,24 @@ static void basics(struct slab *s) {
 
     for (int i = 0; i < M; i++) {
         void *val = NULL;
-        expect( slab_lookup(s, jenny[i], &val, key_eq,NULL) && val == jenny+i);
+        expect( slab_lookup(s, jenny[i], &val, key_eq,ctx) && val == jenny+i);
     }
     for (int i = M; i < len(jenny); i++) {
         void *val = NULL;
-        expect(!slab_lookup(s, jenny[i], &val, key_eq,NULL) && val == NULL);
+        expect(!slab_lookup(s, jenny[i], &val, key_eq,ctx) && val == NULL);
     }
 }
 
 static void slab_holds_null(struct slab *s) {
     void *val = s;
-    expect(!slab_lookup(s,42,&val, key_eq,NULL) && val == s);
+    expect(!slab_lookup(s,42,&val, key_eq,ctx) && val == s);
     expect( slab_insert(s,42,NULL) && slab_len(s) == 1);
-    expect( slab_lookup(s,42,&val, key_eq,NULL) && val == NULL);
+    expect( slab_lookup(s,42,&val, key_eq,ctx) && val == NULL);
 }
 
 static void slab_holds_self(struct slab *s) {
     void *val = NULL;
-    expect(!slab_lookup(s,(intptr_t)s,&val, key_eq,NULL) && val == NULL);
+    expect(!slab_lookup(s,(intptr_t)s,&val, key_eq,ctx) && val == NULL);
 
     int prev=0, len=0;
     while (slab_insert(s,(intptr_t)s,s)) {
@@ -55,25 +56,25 @@ static void slab_holds_self(struct slab *s) {
     }
     expect(len == prev+1);
 
-    expect(slab_lookup(s,(intptr_t)s,&val, key_eq,NULL) && val == s);
+    expect(slab_lookup(s,(intptr_t)s,&val, key_eq,ctx) && val == s);
 }
 
 static void slab_holds_its_own_function_pointers(struct slab *s) {
     void *val = NULL;
-    expect(!slab_lookup(s, 1, &val, key_eq,NULL) && val == NULL);
-    expect(!slab_lookup(s, 2, &val, key_eq,NULL) && val == NULL);
-    expect(!slab_lookup(s, 3, &val, key_eq,NULL) && val == NULL);
-    expect(!slab_lookup(s, 4, &val, key_eq,NULL) && val == NULL);
+    expect(!slab_lookup(s, 1, &val, key_eq,ctx) && val == NULL);
+    expect(!slab_lookup(s, 2, &val, key_eq,ctx) && val == NULL);
+    expect(!slab_lookup(s, 3, &val, key_eq,ctx) && val == NULL);
+    expect(!slab_lookup(s, 4, &val, key_eq,ctx) && val == NULL);
 
     slab_insert(s, 1, (void*)slab_insert);
     slab_insert(s, 2, (void*)slab_lookup);
     slab_insert(s, 3, (void*)slab_len);
     slab_insert(s, 4, (void*)slab_alloc);
 
-    expect(slab_lookup(s, 1, &val, key_eq,NULL) && val == (void*)slab_insert);
-    expect(slab_lookup(s, 2, &val, key_eq,NULL) && val == (void*)slab_lookup);
-    expect(slab_lookup(s, 3, &val, key_eq,NULL) && val == (void*)slab_len);
-    expect(slab_lookup(s, 4, &val, key_eq,NULL) && val == (void*)slab_alloc);
+    expect(slab_lookup(s, 1, &val, key_eq,ctx) && val == (void*)slab_insert);
+    expect(slab_lookup(s, 2, &val, key_eq,ctx) && val == (void*)slab_lookup);
+    expect(slab_lookup(s, 3, &val, key_eq,ctx) && val == (void*)slab_len);
+    expect(slab_lookup(s, 4, &val, key_eq,ctx) && val == (void*)slab_alloc);
 }
 
 int main(void) {
